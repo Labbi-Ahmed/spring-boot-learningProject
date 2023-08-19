b4d4addb9c83d55354ea316758960e0b4294b7f6
Labbi-Ahmed <labbi5689@gmail.com>
2023-08-19 16:47:48 +0600

    Play with some properties

--- myFirst-WebApp/src/main/java/com/learnwebapp/controller/MyController.java
+++ myFirst-WebApp/src/main/java/com/learnwebapp/controller/MyController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 
+
+@Controller
 public class MyController {
-	@GetMapping("/say-hello")
+	@GetMapping("say-hello")
 	@ResponseBody
 	public String sayHello() {
-		return "Hello! Labbi Ahmed";
+		
+		return "Say Hello";
 	}
 }
