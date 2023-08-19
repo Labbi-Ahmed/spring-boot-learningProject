36a8731df53138f094985434410a90f130ca80b7
Labbi-Ahmed <labbi5689@gmail.com>
2023-08-19 18:01:06 +0600

    using jsp in this project. config the file structure and make a new file sayhello.jsp

--- myFirst-WebApp/src/main/java/com/learnwebapp/controller/MyController.java
+++ myFirst-WebApp/src/main/java/com/learnwebapp/controller/MyController.java
@@ -14,4 +14,23 @@ public class MyController {
 		
 		return "Say Hello";
 	}
+	
+	@GetMapping("say-hello-html")
+	@ResponseBody
+	public String sayHello_html() {
+		
+		StringBuffer sp = new StringBuffer();
+		
+		sp.append("<html><head><title>This is my page</title></head><body>HI</body></html>");
+		
+		return sp.toString();
+	}
+	
+	
+	@GetMapping("say-hello-jsp")
+	public String sayHelloJSP() {
+		
+		return "sayHello";
+	}
+	
 }
---
+++ myFirst-WebApp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title> This is from my jsp page</title>
+	</head>
+	<body>
+	     <h1 style="text-align=center">add jsp in my project.</h1>
+	</body>
+</html>
