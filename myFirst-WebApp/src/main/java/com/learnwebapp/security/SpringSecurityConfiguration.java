bc9e2184f53466821420a9e146e23b193f71c1d0
Labbi-Ahmed <labbi5689@gmail.com>
2023-09-11 22:38:11 +0600

    save the docker with new config

--- myFirst-WebApp/src/main/java/com/learnwebapp/security/SpringSecurityConfiguration.java
+++ myFirst-WebApp/src/main/java/com/learnwebapp/security/SpringSecurityConfiguration.java
@@ -54,8 +54,7 @@ public class SpringSecurityConfiguration {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(
-				auth -> auth.anyRequest().authenticated());
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
 		
 		http.formLogin(withDefaults());
 		
