16dd32f98c0b38077b3251e7e20278e8f34d4052
Labbi-Ahmed <labbi5689@gmail.com>
2023-09-12 14:49:43 +0600

    uncomment myController.java for testing

--- myFirst-WebApp/src/main/java/com/learnwebapp/MyFirstWebAppApplication.java
+++ myFirst-WebApp/src/main/java/com/learnwebapp/MyFirstWebAppApplication.java
@@ -2,10 +2,17 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyFirstWebAppApplication {
+public class MyFirstWebAppApplication extends SpringBootServletInitializer {
 
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(MyFirstWebAppApplication.class);
+//	}
+//	
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstWebAppApplication.class, args);
 	}
--- myFirst-WebApp/src/main/java/com/learnwebapp/helloController/MyController.java
+++ myFirst-WebApp/src/main/java/com/learnwebapp/helloController/MyController.java
@@ -1,36 +1,36 @@
-//package com.learnwebapp.helloController;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//
-//
-//@Controller
-//public class MyController {
-//	@GetMapping("say-hello")
-//	@ResponseBody
-//	public String sayHello() {
-//		
-//		return "Say Hello";
-//	}
-//	
-//	@GetMapping("say-hello-html")
-//	@ResponseBody
-//	public String sayHello_html() {
-//		
-//		StringBuffer sp = new StringBuffer();
-//		
-//		sp.append("<html><head><title>This is my page</title></head><body>HI</body></html>");
-//		
-//		return sp.toString();
-//	}
-//	
-//	
-//	@GetMapping("say-hello-jsp")
-//	public String sayHelloJSP() {
-//		
-//		return "sayHello";
-//	}
-//	
-//}
+package com.learnwebapp.helloController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class MyController {
+	@GetMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		
+		return "Say Hello";
+	}
+	
+	@GetMapping("say-hello-html")
+	@ResponseBody
+	public String sayHello_html() {
+		
+		StringBuffer sp = new StringBuffer();
+		
+		sp.append("<html><head><title>This is my page</title></head><body>HI</body></html>");
+		
+		return sp.toString();
+	}
+	
+	
+	@GetMapping("say-hello-jsp")
+	public String sayHelloJSP() {
+		
+		return "sayHello";
+	}
+	
+}
